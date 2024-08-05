0b59d4fc7be29c3987b4855b6191f9fa2c0dcb11
tatsuya-asami <asami.tatsuya@commmune.jp>
2024-08-05 23:08:24 +0900

    chore: Add type generation for target variables in generate-type-and-set.ts

--- scripts/generate-type-and-set.ts
+++ scripts/generate-type-and-set.ts
@@ -1,10 +1,44 @@
-import { Project } from "ts-morph";
+import {
+  OptionalKind,
+  Project,
+  SourceFile,
+  TypeAliasDeclarationStructure,
+} from "ts-morph";
 
 const project = new Project({
   tsConfigFilePath: "./tsconfig.json",
 });
 const sourceFiles = project.addSourceFilesAtPaths("src/**/*.ts");
 
+const createTypeAndSet = (sourceFile: SourceFile) => {
+  const targetVariable = sourceFile.getVariableDeclarations()?.[0];
+  if (!targetVariable) {
+    console.log("No target variables found");
+    return;
+  }
+  const isTypeArray = targetVariable.getType().isArray();
+  const elementType = isTypeArray
+    ? targetVariable.getType().getArrayElementType()
+    : targetVariable.getType();
+  if (!elementType) {
+    console.log("No element type found");
+    return;
+  }
+
+  const typeAlias: OptionalKind<TypeAliasDeclarationStructure> = {
+    name: `${targetVariable.getName()[0].toUpperCase()}${targetVariable
+      .getName()
+      .slice(1)}`,
+    type: elementType.getText(),
+  };
+
+  sourceFile.insertTypeAlias(0, typeAlias);
+  targetVariable.setType(isTypeArray ? `${typeAlias.name}[]` : typeAlias.name);
+};
+
 sourceFiles.forEach((sourceFile) => {
-  console.log(sourceFile.getFilePath());
+  createTypeAndSet(sourceFile);
+  sourceFile.saveSync();
 });
+
+project.saveSync();
